b112a10ed063bff9811d61d5835cde9c7aba4c96
duongni <133175915+duongni@users.noreply.github.com>
2023-05-25 17:15:52 -0400

    added function for selection dropdown

--- style/index.js
+++ style/index.js
@@ -1,27 +1,56 @@
 function updateTime() {
   let currentLaDate = document.querySelector("#la-date");
-  let laTime = document.querySelector("#la-current-time");
-  let laTimeZone = moment().tz("America/Los_Angeles");
-  currentLaDate.innerHTML = laTimeZone.format("MMMM Do YYYY");
-  laTime.innerHTML = laTimeZone.format("hh:mm:ss[<span>]A[</span>]");
+  if (currentLaDate) {
+    let laTime = document.querySelector("#la-current-time");
+    let laTimeZone = moment().tz("America/Los_Angeles");
+    currentLaDate.innerHTML = laTimeZone.format("MMMM Do YYYY");
+    laTime.innerHTML = laTimeZone.format("hh:mm:ss[<span>]A[</span>]");
+  }
 
   let currentSydneyDate = document.querySelector("#sydney-date");
-  let sydneyTime = document.querySelector("#sydney-current-time");
-  let sydneyTimeZone = moment().tz("Australia/Sydney");
-  currentSydneyDate.innerHTML = sydneyTimeZone.format("MMMM Do YYYY");
-  sydneyTime.innerHTML = sydneyTimeZone.format("hh:mm:ss[<span>]A[</span>]");
-
-  let currentTaipeiDate = document.querySelector("#taipei-date");
-  let taipeiTime = document.querySelector("#taipei-current-time");
-  let taipeiTimeZone = moment().tz("Asia/Taipei");
-  currentTaipeiDate.innerHTML = taipeiTimeZone.format("MMMM Do YYYY");
-  taipeiTime.innerHTML = taipeiTimeZone.format("hh:mm:ss[<span>]A[</span>]");
+  if (currentSydneyDate) {
+    let sydneyTime = document.querySelector("#sydney-current-time");
+    let sydneyTimeZone = moment().tz("Australia/Sydney");
+    currentSydneyDate.innerHTML = sydneyTimeZone.format("MMMM Do YYYY");
+    sydneyTime.innerHTML = sydneyTimeZone.format("hh:mm:ss[<span>]A[</span>]");
+  }
 
   let currentSpainDate = document.querySelector("#spain-date");
-  let spainTime = document.querySelector("#spain-current-time");
-  let spainTimeZone = moment().tz("America/Port_of_Spain");
-  currentSpainDate.innerHTML = spainTimeZone.format("MMMM Do YYYY");
-  spainTime.innerHTML = spainTimeZone.format("hh:mm:ss[<span>]A[</span>]");
+  if (currentSpainDate) {
+    let spainTime = document.querySelector("#spain-current-time");
+    let spainTimeZone = moment().tz("America/Port_of_Spain");
+    currentSpainDate.innerHTML = spainTimeZone.format("MMMM Do YYYY");
+    spainTime.innerHTML = spainTimeZone.format("hh:mm:ss[<span>]A[</span>]");
+  }
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#city");
+  citiesElement.innerHTML = `
+  <div class="row">
+          <div class="col-6">
+            <h3 class="city" id="Australia/Sydney">${cityName}</h3>
+            <h7 class="current-date" id="sydney-date">${cityTime.format(
+              "MMMM Do YYYY"
+            )}</h7>
+          </div>
+          <div class="col-6" class="time-element">
+            <h2 class="current-time" id="sydney-current-time">${cityTime.format(
+              "hh:mm:ss"
+            )}<span>${cityTime.format("A")}</span>
+            </h2>
+          </div>
+        </div>
+    `;
 }
 
 setInterval(updateTime, 1000);
+
+let citySelectionElement = document.querySelector("#cities");
+citySelectionElement.addEventListener("change", updateCity);
